25c00a7e9c65b75851383861e03db4e162064a02
qzhu496 <qzhu496@FCLD02.sfac.auckland.ac.nz>
2013-05-08 14:55:57 +1200

    some progress

--- DHT.java
+++ DHT.java
@@ -1,7 +1,28 @@
-
 public class DHT {
-
-	public DHT(int n){
+	
+	private SimpleHT[] sHTs;
+	private int upTo = 0;
+	
+	
+	public DHT(int n) {
+		sHTs = new SimpleHT[n];
+		for (int i = 0; i < n; i++){
+			sHTs[i] = new SimpleHT();
+		}
+	}
+	Integer place(String key){
+		return Integer.parseInt(key.substring(key.lastIndexOf('.') + 1)) % 8;
+	}
+	
+	public void lookup(String key){
 		
+		sHTs[place(key)].lookup(key);
+	}
+	
+	public void statistics(){
+		for (int i = 0; i < sHTs.length; i++){
+			//sHTs[i].dump();
+			System.out.println("ht " + i + " = " + sHTs[i].size());
+		}
 	}
 }
--- N_ht.java
+++ N_ht.java
@@ -1,30 +1,85 @@
 import java.io.*;
 import java.util.Scanner;
 import java.util.NoSuchElementException;
+
 // command test
 public class N_ht {
 
-  public static void main(String[] args) throws IOException {
-    Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		
 
-    Integer nn = 8;
-    DHT dht = new DHT(nn);  // You must write this class!
+		Integer nn = 8;
+		DHT dht = new DHT(nn); // You must write this class!
+/*
+		Scanner scanner = new Scanner(System.in);
+		try {
+			String fqdn, src, key;
+			Integer count;
+			int n = 0;
+			do {
+				fqdn = scanner.next();
+				src = scanner.next();
+				key = fqdn + " " + src;
+				// System.out.printf("%d: %s\n", n, key);
+				dht.lookup(key);
+				n += 1;
+			} while (true); // (n != 5);
+		} catch (NoSuchElementException e) {
+			dht.statistics();
+		}*/
 
-    try {
-      String fqdn, src, key;
-      Integer count;
-      int n = 0;
-      do {
-        fqdn = scanner.next();  src = scanner.next();
-        key = fqdn + " " + src;
-        // System.out.printf("%d: %s\n", n, key);
-        dht.lookup(key);
-        n += 1;
-      } while (true);  // (n != 5);
-    } catch (NoSuchElementException e) {
-      // System.out.printf("end-of-file\n");    
-    }
+		// while (scanner.hasNext()){
+		// System.out.println(scanner.next());
+		// }
+		//
+		String fqdn, src, key;
+		Integer count;
+		int n = 0;
+		
+		
+/*		Scanner scanner = new Scanner(System.in);
+		while (scanner.hasNextLine()) {
+			String nextLine = scanner.nextLine();
+			//System.out.println("nextLine: " + nextLine + nextLine.equals("\n"));
+			Scanner nl = new Scanner(nextLine);
+			
+			while (nl.hasNext()){
+				fqdn = nl.next();
+				src = nl.next();
+				key = fqdn + " " + src;
+				System.out.printf("%d: %s\n", n, key);
+				dht.lookup(key);
+				n += 1;
+			}
+			//while (true){
+				System.out.println(scanner.hasNextLine());
+			//}
+		}*/
+		
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String nextLine;
+		while ((nextLine = br.readLine()) != null && !nextLine.equals("")){
+			System.out.println("nextLine: " + nextLine);
+			Scanner scanner = new Scanner(nextLine);
+			fqdn = scanner.next();
+			src = scanner.next();
+			key = fqdn + " " + src;
+			System.out.printf("%d: %s\n", n, key);
+			dht.lookup(key);
 
-    dht.statistics();
-  }
+//			if (n >= 99) {
+//				// System.out.println(scanner.hasNext()?scanner.next():"null");
+//				while (scanner.hasNext()) {
+//					System.out.println(scanner.next());
+//				}
+//			}
+
+			n += 1;
+			//System.out.println("has next line: " + br.)
+		}
+		
+		System.out.println("out");
+		dht.statistics();
+	}
 }
